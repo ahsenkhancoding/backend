b692877319b4a211b4db715d884c2fc044216672
AhsenKhan <ahsenkhanmags@gmail.com>
2026-01-04 01:30:14 +0500

    added pagination to rest framework settings

--- pharmacy_core/settings.py
+++ pharmacy_core/settings.py
@@ -142,6 +142,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+'PAGE_SIZE': 12,
 }
 
 # Simple JWT Configuration
